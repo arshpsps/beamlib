b9f8391cafb6a21615abb215c101e4ad3b5360c6
ArshhGill <arshh.gill@outlook.com>
2024-04-29 06:18:10 +0530

    deltaTime instead of frameCounter

--- internals/main.cpp
+++ internals/main.cpp
@@ -1,28 +1,24 @@
 #include <raylib.h>
 
-const float DRAW_FPS = 120;
+const float DRAW_FPS = 60;
 const float NORMAL_FPS = 30;
-
-int normalizeByFPS(int val) { return val / NORMAL_FPS * DRAW_FPS; }
 
 int main() {
     InitWindow(800, 450, "beamlib");
 
     SetTargetFPS(DRAW_FPS);
-    float ticks = 1;
     Vector2 pos = {800.0 / 2, 450.0 / 2};
 
     while (!WindowShouldClose()) {
+        float deltaTime = GetFrameTime();
         BeginDrawing();
         {
             ClearBackground(BLACK);
             DrawText("This is beamlib", 190, 200, 20, LIGHTGRAY);
-            if (DRAW_FPS / ticks == NORMAL_FPS) {
-                ticks = 0;
-                pos = {pos.x + 2, pos.y};
-            }
+
+            pos = {pos.x + 100 * deltaTime, pos.y};
+
             DrawPoly(pos, 4, 10.0, 45, LIGHTGRAY);
-            ticks++;
             DrawFPS(5, 5);
         }
         EndDrawing();
