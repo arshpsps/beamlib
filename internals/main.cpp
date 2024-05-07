e36f39a24e1d12a2562e609d48e2f760495a4510
ArshhGill <arshh.gill@outlook.com>
2024-05-07 19:55:38 +0530

    window size

--- internals/main.cpp
+++ internals/main.cpp
@@ -2,9 +2,18 @@
 
 const float DRAW_FPS = 60;
 const float NORMAL_FPS = 30;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 450;
 
 int main() {
-    InitWindow(800, 450, "beamlib");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "beamlib");
+    Rectangle box = {400, 280, 40, 40};
+
+    Camera2D camera = {0};
+    camera.target = (Vector2){box.x + 20.0f, box.y + 20.0f};
+    camera.offset = (Vector2){SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f};
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
 
     SetTargetFPS(DRAW_FPS);
     Vector2 pos = {800.0 / 2, 450.0 / 2};
@@ -14,7 +23,11 @@ int main() {
         BeginDrawing();
         {
             ClearBackground(BLACK);
-            DrawText("This is beamlib", 190, 200, 20, LIGHTGRAY);
+            const char *textToWrite = "UwU, idk why there's space up there";
+            DrawText(textToWrite,
+                     (GetScreenWidth() / 2) -
+                         (MeasureText(textToWrite, 20) / 2),
+                     (GetScreenHeight() / 2), 20, LIGHTGRAY);
 
             pos = {pos.x + 100 * deltaTime, pos.y};
 
